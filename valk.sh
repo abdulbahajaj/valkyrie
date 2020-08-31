3df32cebe504c745f74e3d70110c4f773b9b3260
NataliaP721 <natalia.pavlovic@ucalgary.ca>
2020-08-31 02:02:42 -0400

    Added repl.it integration

--- valk.sh
+++ valk.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-# Compiler entry point
+# Set to true if running in repl.it
+replit=true
+
+Compiler entry point
 rm -rf out
 git clone https://github.com/abdulbahajaj/valk-runtime.git out
 clj valkyrie.clj $@
-cd out && make build_server
+
+if [ "$replit" = true ] ; then
+  install-pkg leiningen
+  rm /home/runner/.apt/usr/bin/lein
+  cp /home/runner/valkyrie/lein /home/runner/.apt/usr/bin/lein
+  chmod a+x /home/runner/.apt/usr/bin/lein
+fi
+
+cd out && make build_server && make run_server
